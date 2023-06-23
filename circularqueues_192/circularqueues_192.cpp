873add0fda9383326ee16902ef173bd4133d874b
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-23 12:59:05 +0700

    return

--- circularqueues_192/circularqueues_192.cpp
+++ circularqueues_192/circularqueues_192.cpp
@@ -19,4 +19,7 @@ public:
 		cout << endl;
 
 		// Cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1)) {
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
