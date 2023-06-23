ffb72d8be903ea77854254fd292c5dd90f645c03
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-23 13:23:53 +0700

    front position

--- circularqueues_192/circularqueues_192.cpp
+++ circularqueues_192/circularqueues_192.cpp
@@ -81,6 +81,14 @@ public:
 				FRONT_position++;
 			}
 			cout << endl;
+		}
+		else {
+			//jika Front_position > Rear_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "    ";
+				FRONT_position++;
+			}
+			FRONT_position = 0;
 
 
 
