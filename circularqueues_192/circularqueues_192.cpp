87d7bf439563824116d30a18cbf7a52a389386d6
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-23 13:26:56 +0700

    endline;

--- circularqueues_192/circularqueues_192.cpp
+++ circularqueues_192/circularqueues_192.cpp
@@ -90,5 +90,12 @@ public:
 			}
 			FRONT_position = 0;
 
+			//iterasi dari awal array hingga rear
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+
 
 
