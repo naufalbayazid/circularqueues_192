c3cdb25ed183984bbfa0436bf332d135cbdaece1
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-23 13:08:56 +0700

    mengecek antriann hanya memiliki satu element

--- circularqueues_192/circularqueues_192.cpp
+++ circularqueues_192/circularqueues_192.cpp
@@ -47,3 +47,10 @@ public:
 			return;
 		}
 		cout << "THE element deleted from the queue is : " << queue_array[FRONT] << "\n";
+
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+
+		}
